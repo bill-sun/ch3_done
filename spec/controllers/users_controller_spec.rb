f0a09676e06657790598749dd8c1d64320876eca
bill <bill@ruby.com>
2016-01-18 11:38:38 +0800

    Finished chp 05

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  render_views
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+	it "should have the right title" do
+	  get 'new'
+	  expect(response.body).to have_selector("title",
+								             :text => "Sign up",
+                                             :visible => false)
+
+	end
+    
+  end
+
+end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -13,25 +13,26 @@ RSpec.describe "LayoutLinks", type: :request do
 
 
     it "returns http success" do
-      get home_path
       get '/'
       expect(response).to have_http_status(:success)
     end
     it "returns http success" do
-      get contact_path
       get '/contact'
       expect(response).to have_http_status(:success)
     end
     it "returns http success" do
-      get about_path
       get '/about'
       expect(response).to have_http_status(:success)
     end
     it "returns http success" do
-      get help_path
       get '/help'
       expect(response).to have_http_status(:success)
     end
+	it "should have a signup page at '/signup'" do
+	  get '/signup'
+      expect(response).to have_http_status(:success)
+	end
+    
   end
   
 end
